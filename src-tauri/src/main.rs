5a6659c5c22bfe6271d91db02a0f70a8f4a00576
Philipp Walter <philippwalter@pm.me>
2022-02-20 17:18:50 +0100

    add tauri-plugin-authenticator

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,10 +6,12 @@
 mod commands;
 
 use tauri_plugin_stronghold::TauriStronghold;
+use tauri_plugin_authenticator::TauriAuthenticator;
 
 fn main() {
   tauri::Builder::default()
     .plugin(TauriStronghold::default())
+    .plugin(TauriAuthenticator::default())
     .invoke_handler(tauri::generate_handler![commands::generate_totp])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
