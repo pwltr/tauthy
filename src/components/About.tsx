ddc13f8fdd781f2bca72ab729d2585b665f6b62f
Philipp Walter <philippwalter@pm.me>
2022-03-06 01:05:02 +0100

    feat: update About

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useContext } from 'react'
 import { open } from '@tauri-apps/api/shell'
+import { styled } from '@mui/material/styles'
 import List from '@mui/material/List'
 import ListItemButton from '@mui/material/ListItemButton'
 import ListItemText from '@mui/material/ListItemText'
@@ -8,10 +9,21 @@ import ListSubheader from '@mui/material/ListSubheader'
 import CodeIcon from '@mui/icons-material/Code'
 import HistoryEduIcon from '@mui/icons-material/HistoryEdu'
 import GitHubIcon from '@mui/icons-material/GitHub'
+import BugReportIcon from '@mui/icons-material/BugReport'
 import VolunteerActivismIcon from '@mui/icons-material/VolunteerActivism'
 
 import { AppBarTitleContext } from '~/context'
 import ListItem from '~/components/ListItem'
+
+const Header = styled('div')`
+  display: flex;
+  align-items: center;
+  grid-gap: 1rem;
+  font-size: 1.3rem;
+  padding-top: 1rem;
+  padding-left: 1rem;
+  padding-bottom: 0.3rem;
+`
 
 const About = () => {
   const { setAppBarTitle } = useContext(AppBarTitleContext)
@@ -22,15 +34,31 @@ const About = () => {
 
   return (
     <>
-      <List dense>
-        {/* <ListItem>
+      <Header>
+        <img src="../assets/tauthy_bordered.png" width={67} />
+        Tauthy
+      </Header>
+
+      <List>
+        <ListSubheader>Open Source</ListSubheader>
+        <ListItem disablePadding onClick={() => open('https://github.com/pwltr/tauthy')}>
           <ListItemButton>
             <ListItemIcon>
-              <DraftsIcon color="primary" />
+              <GitHubIcon color="primary" />
             </ListItemIcon>
-            <ListItemText primary="Tauthy" />
+            <ListItemText primary="GitHub" secondary="pwltr/tauthy" />
           </ListItemButton>
-        </ListItem> */}
+        </ListItem>
+
+        <ListItem disablePadding onClick={() => open('https://github.com/pwltr/tauthy/issues')}>
+          <ListItemButton>
+            <ListItemIcon>
+              <BugReportIcon color="primary" />
+            </ListItemIcon>
+            <ListItemText primary="Report a Bug" secondary="Open an issue on GitHub" />
+          </ListItemButton>
+        </ListItem>
+
         <ListItem disablePadding onClick={() => open('https://github.com/pwltr/tauthy/releases')}>
           <ListItemButton>
             <ListItemIcon>
@@ -49,16 +77,6 @@ const About = () => {
               <HistoryEduIcon color="primary" />
             </ListItemIcon>
             <ListItemText primary="License" secondary="GPL-3.0 License" />
-          </ListItemButton>
-        </ListItem>
-
-        <ListSubheader>Open Source</ListSubheader>
-        <ListItem disablePadding onClick={() => open('https://github.com/pwltr/tauthy')}>
-          <ListItemButton>
-            <ListItemIcon>
-              <GitHubIcon color="primary" />
-            </ListItemIcon>
-            <ListItemText primary="GitHub" secondary="pwltr/tauthy" />
           </ListItemButton>
         </ListItem>
 
