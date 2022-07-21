d640e641a8d8d08f0e7404dae63fe8d8b6f3506f
Philipp Walter <philippwalter@pm.me>
2022-07-21 18:04:17 +0200

    feat: use lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  'src/**/*.{ts,tsx}': [
-    'yarn format',
-    'yarn lint:check',
-  ],
+  'src/**/*.{ts,tsx}': ['yarn format', 'yarn lint:check'],
+  '{.{eslintrc.js,prettierrc}}': ['prettier --parser json --write'],
+  '*.{yml,md}': ['prettier --single-quote --write'],
 }
