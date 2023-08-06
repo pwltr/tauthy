128611e7af2460c135045c14e714c986a5b246cd
Philipp Walter <philippwalter@proton.me>
2023-08-06 02:14:23 +0200

    chore: bump version

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -70,7 +70,7 @@ const About = () => {
             <ListItemIcon>
               <CodeIcon color="primary" />
             </ListItemIcon>
-            <ListItemText primary={t('about.version')} secondary="0.2.5" />
+            <ListItemText primary={t('about.version')} secondary="0.2.6" />
           </ListItemButton>
         </ListItem>
 
